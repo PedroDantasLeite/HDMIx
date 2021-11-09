8c38ce7cf4c316005f5515ffc31cbc78024db8e4
Pedro Leite <83736602+PedroDantasLeite@users.noreply.github.com>
2021-11-09 14:21:20 -0300

    update feedBack()

--- hdmix.h
+++ hdmix.h
@@ -1,5 +1,5 @@
 void addLogin (char *nome, char *snome, char *senha, char *email, char *idade);
 int login (char *email, char *senha);
-void feedBack (char *email);
+void feedBack (char *email, char *feedback);
 int exLogin (char *email);
 void verPerf (char *email);
